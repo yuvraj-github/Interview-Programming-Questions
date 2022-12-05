4de4fcec0e45d2fd45858c675278499ad2ffd9f6
yuvraj <yuvi.unique@gmail.com>
2022-12-05 20:24:56 +0530

    Added towers of hanoi problem

---
+++ Practices/Recursion/Towers of hanoi/Solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void towerOfHanoi(int n, char s, char h, char d)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    if (n == 1)
+    {
+        cout << s << " " << d << endl;
+        return;
+    }
+    towerOfHanoi(n - 1, s, d, h);
+    cout << s << " " << d << endl;
+    towerOfHanoi(n - 1, h, s, d);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    towerOfHanoi(n, 'a', 'b', 'c');
+}
