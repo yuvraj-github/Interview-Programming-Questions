fc4c272292d39a46dd1522ff432d5ccdf92e3b4c
yuvraj <yuvi.unique@gmail.com>
2022-12-11 19:04:43 +0530

    Added new programs

--- Practices/Recursion/Return permutations of a string/Solution.cpp
+++ Practices/Recursion/Return permutations of a string/Solution.cpp
@@ -6,16 +6,21 @@ void generatePermutationsHelper(string &str, vector<string> &ans, string output)
 {
     if (str.length() == 0)
     {
-        ans.push_back(output);
+         ans.push_back(output);
+        // if (std::find(ans.begin(), ans.end(), output) == ans.end()) // check output exists in the vector.
+        // {
+        //     //output not exists.
+        //    ans.push_back(output);
+        // }
         return;
     }
     for (int i = 0; i < str.length(); i++)
     {
         char ch = str[i];
         string leftPart = str.substr(0, i);
-        string rightPart = str.substr(i+1);
+        string rightPart = str.substr(i + 1);
         string resultStr = leftPart + rightPart;
-        generatePermutationsHelper(resultStr, ans, output+ch);
+        generatePermutationsHelper(resultStr, ans, output + ch);
     }
 }
 vector<string> generatePermutations(string &str)
@@ -23,7 +28,7 @@ vector<string> generatePermutations(string &str)
     // write your code here
     vector<string> ans;
     generatePermutationsHelper(str, ans, "");
-    sort(ans.begin(), ans.end());   
+    sort(ans.begin(), ans.end());
     return ans;
 }
 int main()
--- Practices/Recursion/Return permutations of a string/tempCodeRunnerFile.cpp
+++ Practices/Recursion/Return permutations of a string/tempCodeRunnerFile.cpp
@@ -1,2 +1,44 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-        string rightPart = str.substr(i+1);
+void generatePermutationsHelper(string &str, vector<string> &ans, string output)
+{
+    if (str.length() == 0)
+    {
+        // ans.push_back(output);
+        if (std::find(ans.begin(), ans.end(), output) != ans.end())
+        {
+            // elem exists in the vector
+            ans.push_back(answer);
+        }
+        return;
+    }
+    for (int i = 0; i < str.length(); i++)
+    {
+        char ch = str[i];
+        string leftPart = str.substr(0, i);
+        string rightPart = str.substr(i + 1);
+        string resultStr = leftPart + rightPart;
+        generatePermutationsHelper(resultStr, ans, output + ch);
+    }
+}
+vector<string> generatePermutations(string &str)
+{
+    // write your code here
+    vector<string> ans;
+    generatePermutationsHelper(str, ans, "");
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+int main()
+{
+    string str;
+    cin >> str;
+    vector<string> output = generatePermutations(str);
+    for (int i = 0; i < output.size(); i++)
+    {
+        cout << output[i] << endl;
+    }
+    return 0;
+}
