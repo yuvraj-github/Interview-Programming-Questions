aa36a1fd601b8eebe999ecb77556b8df81082c50
yuvraj <yuvi.unique@gmail.com>
2022-12-12 20:46:13 +0530

    Added new assignments

---
+++ Practices/Others/Duplicate in array/Solution.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int findDuplicate(int *arr, int n)
+{
+    // Write your code here
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    for (int i = 1; i < n; i++)
+    {
+        ans = ans ^ i;
+    }
+    return ans;
+}
+int main()
+{
+
+    int size;
+    cin >> size;
+    int *input = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> input[i];
+    }
+
+    cout << findDuplicate(input, size) << endl;
+
+    return 0;
+}
