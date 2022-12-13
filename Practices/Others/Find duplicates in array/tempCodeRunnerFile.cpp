df5fe4c41e373b71db224952f999e34004e25ea2
yuvraj <yuvi.unique@gmail.com>
2022-12-13 19:35:32 +0530

    Added new programs

---
+++ Practices/Others/Find duplicates in array/tempCodeRunnerFile.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> duplicates(int arr[], int n)
+{
+    // code here
+    vector<int> ans;
+    for (int i = 0; i < n; i++)
+    {
+        int index = arr[i];
+        arr[index] += n;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] / n >= 2)
+        {
+            ans.push_back(i);
+        }
+    }
+    if (ans.size() == 0)
+    {
+        return {-1};
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    vector<int> ans = duplicates(a, n);
+    for (int i : ans)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
