a4b09bc50017f771034339f253ae94bbf9b7578b
yuvraj <yuvi.unique@gmail.com>
2022-12-16 20:44:21 +0530

    Added new assignments

---
+++ Practices/Others/Distinct pairs difference with k/Solution.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int TotalPairs(vector<int> nums, int k)
+{
+    // Code here
+    sort(nums.begin(), nums.end(), greater<int>());
+    unordered_map<int, int> m;
+    int count = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int val = k + nums[i];
+        if (m[val] && m[val] > -1)
+        {
+            count++;
+            m[val] = -1; // set frequency of value -1 for distinct values.
+        }
+        if (!m[nums[i]]) // check already exists value.
+        {
+            m[nums[i]]++;
+        }
+    }
+    return count;
+}
+int main()
+{
+    int n, k, x;
+    cin >> n;
+    vector<int> nums;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        nums.push_back(x);
+    }
+    cin >> k;
+    cout << TotalPairs(nums, k) << endl;
+}
--- Practices/Others/Pairs with difference k/Solution.cpp
+++ Practices/Others/Pairs with difference k/Solution.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int getPairsWithDifferenceK(int *arr, int n, int k)
 {
     unordered_map<int, int> m;
-    sort(arr, arr+n, greater<int>()); // sort array in descending order.
+    sort(arr, arr + n, greater<int>()); // sort array in descending order.
     int count = 0;
     for (int i = 0; i < n; i++)
     {
@@ -27,7 +27,7 @@ int main()
     {
         cin >> arr[i];
     }
-    cin>>k;
+    cin >> k;
     cout << getPairsWithDifferenceK(arr, n, k);
     delete[] arr;
 }
