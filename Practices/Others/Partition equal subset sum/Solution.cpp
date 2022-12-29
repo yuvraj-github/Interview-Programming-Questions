6ab4594d7ac342ff2dd31d3e2a776fae0ae1b9cb
yuvraj <yuvi.unique@gmail.com>
2022-12-29 21:58:06 +0530

    Added partition equal subset sum program memoization concept

--- Practices/Others/Partition equal subset sum/Solution.cpp
+++ Practices/Others/Partition equal subset sum/Solution.cpp
@@ -20,9 +20,33 @@ bool splitArrayHelper(int arr[], int n, int index, int target)
     bool exc = splitArrayHelper(arr, n, index + 1, target);
     return inc || exc;
 }
+// using memoization. (top down approach)
+bool splitArrayHelperMem(int arr[], int n, int index, int target, vector<vector<int>> &dp)
+{
+    if (index >= n)
+    {
+        return false;
+    }
+    if (target < 0)
+    {
+        return false;
+    }
+    if (target == 0)
+    {
+        return true;
+    }
+    if (dp[index][target] != -1)
+    {
+        return dp[index][target];
+    }
+    bool inc = splitArrayHelperMem(arr, n, index + 1, target - arr[index], dp);
+    bool exc = splitArrayHelperMem(arr, n, index + 1, target, dp);
+    return dp[index][target] = inc || exc;
+}
 bool splitArray(int arr[], int n)
 {
-    if(n<=1) {
+    if (n <= 1)
+    {
         return false;
     }
     int total = 0;
@@ -30,13 +54,14 @@ bool splitArray(int arr[], int n)
     {
         total += arr[i];
     }
-    cout<<total<<endl;
-    if (total & 1)
+    if (total & 1) // check odd number
     {
         return false;
     }
     int target = total / 2;
-    return splitArrayHelper(arr, n, 0, target);
+    // return splitArrayHelper(arr, n, 0, target);
+    vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+    return splitArrayHelperMem(arr, n, 0, target, dp);
 }
 int main()
 {
